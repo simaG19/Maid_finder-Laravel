44d4c1fb84816c84fc1b6fa3709b6c0143a33b06
simon <simonwubshet254@gmail.com>
2024-08-28 21:36:28 +0300

    routes\web.php

--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -22,3 +22,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+use App\Http\Controllers\HomeController;
+
+Route::get('/home', [HomeController::class, 'index']);
