ca3891b418458b9f528f21d784366345e74f0ce8
simon <simonwubshet254@gmail.com>
2024-08-28 21:35:30 +0300

    dashboard

--- myproject/resources/views/dashboard.blade.php
+++ myproject/resources/views/dashboard.blade.php
@@ -9,7 +9,10 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <p>hello</p>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+            
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
