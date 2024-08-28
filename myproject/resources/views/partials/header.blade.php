c78e8d0f97f2a83453f817f5c52442b23e75e8f0
simon <simonwubshet254@gmail.com>
2024-08-28 21:36:13 +0300

    header.blade.php

---
+++ myproject/resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<div class="head">    
+    <div class="nav">
+        <h1 class="logo">Maid<span>SITE</span></h1>
+        <ul style="margin-left: 70%;">
+            <li><a href="{{ url('login_form') }}">Login</a></li>
+            <li><a href="{{ url('register_form') }}">Register</a></li>
+            <li><a href="{{ url('about') }}">About</a></li>
+        </ul>
+    </div>
+</div>
